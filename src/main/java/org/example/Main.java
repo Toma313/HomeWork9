ffa04e13c660fe8dba55a5c535140481405116fa
tamara.voloshchuk <tamara.voloshchuk01@gmail.com>
2022-12-14 15:48:56 +0200

    OK , works

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,25 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.*;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<String,String> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key3", "value3");
+        map.put("key2", "value2");
+        map.put("keyn", "valuen");
+        //System.out.println(map);
+        //Set<String> stringSet= new HashSet<>(map.keySet());
+        //System.out.println(stringSet);
+        //Set<String> set =new HashSet<>(map.values());
+        //System.out.println(set);
+       List<String> list = new ArrayList<>(map.values());
+        //System.out.println(list);
+        for (Map.Entry<String, String> pair:map.entrySet()){
+            System.out.println(pair);
+        }
+
     }
-}
+    }
+
