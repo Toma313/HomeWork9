27de38e23987573944df9344905612d27dfba113
tamara.voloshchuk <tamara.voloshchuk01@gmail.com>
2022-12-20 09:04:26 +0200

    according comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,24 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String,String> map = new HashMap<>();
-        map.put("key1", "value1");
-        map.put("key3", "value3");
-        map.put("key2", "value2");
-        map.put("keyn", "valuen");
+        Map<Integer,String> map = new HashMap<>();
+        map.put(8518, "value1");
+        map.put(5223, "value3");
+        map.put(9656, "value2");
+        map.put(7686, "valuen");
         //System.out.println(map);
         //Set<String> stringSet= new HashSet<>(map.keySet());
         //System.out.println(stringSet);
         //Set<String> set =new HashSet<>(map.values());
         //System.out.println(set);
-       List<String> list = new ArrayList<>(map.values());
+      // List<String> list = new ArrayList<>(map.values());
         //System.out.println(list);
-        for (Map.Entry<String, String> pair:map.entrySet()){
-            System.out.println(pair);
+//        for (Map.Entry<Integer, String> pair:map.entrySet()){
+//            System.out.println(pair.sort);
+//        }
+        Set<Integer> set = new TreeSet<>(map.keySet());
+        for(Integer s:set){
+            System.out.println(s+"="+map.get(s));
         }
 
     }
