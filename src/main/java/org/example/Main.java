aa24a2777ed4773e9dbac3e98b03038d9116d659
tamara.voloshchuk <tamara.voloshchuk01@gmail.com>
2022-12-20 09:13:34 +0200

    according comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,13 +9,13 @@ public class Main {
         map.put(5223, "value3");
         map.put(9656, "value2");
         map.put(7686, "valuen");
-        //System.out.println(map);
-        //Set<String> stringSet= new HashSet<>(map.keySet());
-        //System.out.println(stringSet);
-        //Set<String> set =new HashSet<>(map.values());
-        //System.out.println(set);
-      // List<String> list = new ArrayList<>(map.values());
-        //System.out.println(list);
+//        System.out.println(map);
+//        Set<String> stringSet= new HashSet<>(map.keySet());
+//        System.out.println(stringSet);
+//        Set<String> set =new HashSet<>(map.values());
+//        System.out.println(set);
+//         List<String> list = new ArrayList<>(map.values());
+//        System.out.println(list);
 //        for (Map.Entry<Integer, String> pair:map.entrySet()){
 //            System.out.println(pair.sort);
 //        }
@@ -25,5 +25,5 @@ public class Main {
         }
 
     }
-    }
+}
 
